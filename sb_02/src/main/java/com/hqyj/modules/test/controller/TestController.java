f69b246f3def1da0791c335e6982490dd6076e81
ly <ly@DESKTOP-UB0E4R5>
2020-01-07 17:15:43 +0800

    "test"

---
+++ sb_02/src/main/java/com/hqyj/modules/test/controller/TestController.java
@@ -0,0 +1,50 @@
+package com.hqyj.modules.test.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hqyj.vo.ApplicationConfigTestBean;
+@RestController
+//@Controller
+public class TestController {
+	@Value("${server.port}")
+	private int port;
+	@Value("${com.hqyj.age}")
+	private int age;
+	@Value("${com.hqyj.name}")
+	private String name;
+	@Value("${com.hqyj.description}")
+	private String description;
+	@Value("${com.hqyj.random}")
+	private String random;
+	
+	
+	@Autowired
+	private ApplicationConfigTestBean configTestBean;
+	
+	@RequestMapping("/test/getconfig")
+	@ResponseBody
+	public String getconfig() {
+		
+		StringBuffer sb = new StringBuffer();
+		sb.append(port)
+			.append("---").append(name)
+			.append("---").append(age).append("</br>");
+		sb.append(configTestBean.getName())
+			.append("----").append(configTestBean.getAge()).append("</br>");
+		
+		return sb.toString();
+		
+	}
+	
+	@RequestMapping("/test/hello")
+	public String hellojava() {
+		
+		return "hellojava!!!";
+		
+	}
+
+}
